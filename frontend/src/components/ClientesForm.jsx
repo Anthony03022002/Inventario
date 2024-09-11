15e0084cee3fbddb2946721666876ff1fae3151b
metmaqluin <met.malquin@yavirac.edu.ec>
2024-09-11 11:25:22 -0500

    formularioclientes

--- frontend/src/components/ClientesForm.jsx
+++ frontend/src/components/ClientesForm.jsx
@@ -2,11 +2,12 @@
 export function ClientesForm() {
   return (
     <div>
+      <h1>Formulario Clientes</h1>
       <form action="">
         <label htmlFor="">Tipo de identificacion</label>
         <select name="" id="">
             <option value="04">RUC</option>
-            <option value="05">Cedula</option>
+            <option value="05">id</option>
             <option value="06">Pasaporte</option>
             <option value="07">Consumidor Final</option>
             <option value="08">ID Exterior</option>
